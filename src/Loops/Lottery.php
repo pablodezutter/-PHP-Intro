8eca901cb29715f4f60381e010dcb35ba15ddc26
yourname <youremail@address.com>
2019-05-23 11:50:35 +0200

    fit it to another page

--- src/Loops/Lottery.php
+++ src/Loops/Lottery.php
@@ -1,38 +1,52 @@
+<?php require "index2.php" ?>
+<!--This is the view of index2.php -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="mystyle.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    
-    <title>Lottery Game</title>
+
+    <title>Lottery time!</title>
 </head>
 <body>
-    <div class="container">
-
-        <div class="row">
+<div class="container">
+    <div class="row">
             <div class="col">
-            <h1>Play a game</h1>
-</br>
-               <form>
-                <div class="col">  
-                <input name="one"></div>
-                <div class="col"> 
-                <input name="two"></div>
-                <div class="col"> 
-                <input name="three"></div>
-                </form>
-        </div>
-      </div>
-<?php
-                  
-
-
-                  ?>
+                <h1>Play a game</h1>
+                <?php rollDice(); ?>
+                        </br>
+                <p><?= $winner; ?></p> 
+                <p><?= $loser; ?></p>
             </div>
-        </div>
     </div>
+            
+    <div class="row">
+            <div class="col"><button type="button" class="btn btn-danger" id="button">Push to play</button>
+            </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+<script>
+var btn=document.getElementById("button");
+btn.addEventListener("click",function(){
+    location.reload();
+});
+
+
+</script>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
 </body>
+
 </html>
--- src/Loops/index2.php
+++ src/Loops/index2.php
@@ -1,46 +1,30 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="mystyle.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <title>Document</title>
-</head>
-<body>
-      <div class="container">
-        <div class="row">
-            <h1>Play a game</h1>
-</br>
 <?php
+ //require 'Lottery.php';
+ function rollDice(){
 $arrayImages= ['img/burger0.png','img/friet1.png','img/pizza2.png'];
 $loting=[];
 // shuffle($arrayImages); werkt, maar zorgt ervoor dat je nooit 3x dezelfde hebt
   for ($i=0;$i<3;$i++){
 $random=rand(0,2);// kan ook met gekozen image; 
 array_push($loting,$random);
-?>
+
  
-                <div class="col"><img src="<?php echo $arrayImages[$random]; ?>" /></div>
+           echo " <img src=\"". $arrayImages[$random] . "\"/>";
               
- <?php } 
+  } 
         if(sizeof(array_unique($loting))== 1){ //lengte van de lijst met unieke waarden = 1
-         echo "you win";
+         /*$winner*/echo "you win" ."</br>";
         }else{
-            echo "try again?";
+         echo "try again?";
         }
  
-      ?>
- </div>
- </div>
+      
+      }
+?>
      
 
 
 
 
 
-</body>
-</html>
